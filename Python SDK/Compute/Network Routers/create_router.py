027c96c6fcb169099ceb18e2c38ef04848143311
gysahlgreene <eoin.macd95@gmail.com>
2025-11-13 11:19:37 +0000

    create_router

---
+++ Python SDK/Compute/Network Routers/create_router.py
@@ -0,0 +1,40 @@
+# File my_project/create_vrf.py
+
+import os
+from pprint import pprint
+os.environ.setdefault('CLOUDCIX_SETTINGS_MODULE', 'my_settings')
+
+from cloudcix.api.compute import Compute
+
+# Get a util function to get a session using the credentials in your settings file
+from cloudcix.auth import get_admin_token
+
+# ================================================================= #
+#                      Get CloudCIX Token                           #
+# ================================================================= #
+token = get_admin_token()
+
+# VRF (Router) data to create
+vrf_data = {
+    'project_id': 123,          # Change this to your project ID
+    'type': 'router',
+    'name': 'My Test Router',         # Change this to your desired VRF name
+    'networks': [                  # Private networks for this VRF
+        {
+            'name': 'my-network-1',
+            'ipv4': '10.0.1.0/24'
+        },
+        {
+            'name': 'my-network-2', 
+            'ipv4': '10.0.2.0/24'
+        }
+    ]
+}
+
+# ================================================================= #
+#                      Create VRF (Router)                          #
+# ================================================================= #
+response = Compute.network_routers.create(token=token, data=vrf_data)
+
+# Print out the json of the response data with beautiful formatting
+pprint (response.json())
