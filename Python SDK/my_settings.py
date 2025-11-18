fa911c96e53d263e333f8126c8f334d19900d6b7
gysahlgreene <eoin.macd95@gmail.com>
2025-11-18 09:44:09 +0000

    my settings example

---
+++ Python SDK/my_settings.py
@@ -0,0 +1,6 @@
+CLOUDCIX_API_URL="https://legacyapi.api.cloudcix.com/"
+CLOUDCIX_API_V2_URL="https://api.cloudcix.com/"
+CLOUDCIX_API_VERSION="5.0"
+CLOUDCIX_API_USERNAME="example@cix.ie"  # Email Address registered in Prerequisites
+CLOUDCIX_API_PASSWORD="*Password123*"                   # Password for email registered
+CLOUDCIX_API_KEY="mYm3m83R5h1P4P1K3y123"                       # CloudCIX Memebr API Key
